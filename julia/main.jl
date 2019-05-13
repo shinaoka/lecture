d0b5a0ba3802d90084206a440a2b494563da53fc
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2019-05-14 08:55:34 +0900

    Ising model implemented

--- julia/main.jl
+++ julia/main.jl
@@ -1,3 +1,35 @@
+using Random
+
 include("single_spin_flip.jl")
 
-s = SingleSpinFlipUpdater(10)
+function solve()
+    num_spins = 100
+    J = 1.0
+    beta = 100.0
+    num_sweeps = 10
+
+    Jij = Array{Tuple{SpinIndex,SpinIndex,Float64}}(undef, num_spins-1)
+
+    # Nearest neighbor J
+    for ispin = 1:(num_spins-1)
+        Jij[ispin] = (ispin, ispin+1, J)
+    end
+    
+    model = IsingModel(num_spins, Jij)
+    
+    # Create random number generator
+    seed = 1234
+    Random.seed!(seed)
+    
+    updater = SingleSpinFlipUpdater(model)
+    
+    spins = ones(Int, num_spins)
+    energy = compute_energy(model, spins)
+    println(energy)
+    for sweep in 1:10
+        energy += one_sweep(updater, beta, model, spins)
+    end
+    println(energy, "?=", compute_energy(model, spins))
+end
+
+solve()
--- julia/single_spin_flip.jl
+++ julia/single_spin_flip.jl
@@ -1,29 +1,41 @@
-SpinIndex = UInt8
+SpinIndex = Int
+
+struct IsingModel
+    # List of non-zero entries of Jij
+    num_spins::Int
+    Jij::Array{Tuple{SpinIndex,SpinIndex,Float64}}
+end
+
+function compute_energy(model::IsingModel, spins::Array{Int})
+    return -sum([intr[3] * spins[intr[1]] * spins[intr[2]] for intr in model.Jij])
+end
 
 struct SingleSpinFlipUpdater
     num_spins::Int
-    coord_num::Array{UInt8,2}
+    coord_num::Array{Int}
     connection::Array{Tuple{SpinIndex,Float64}}
 
-    function SingleSpinFlipUpdater(num_spins::UInt8, Jij::Array{Tuple{SpinIndex,SpinIndex,Float64},1})
-        println(num_spins)
-        coord_num = zeros(UInt8, num_spins)
+    function SingleSpinFlipUpdater(model::IsingModel)
+        num_spins = model.num_spins
+        Jij = model.Jij
+
+        coord_num = zeros(Int, num_spins)
 
         # Figure out which spins each spin is connected to
-        connection_tmp = [Set{Tuple{SpinIndex,Float64}() for _ in 1:num_spins]
-        num_Jij = Jij.size(1)
+        connection_tmp = [Set{Tuple{SpinIndex,Float64}}() for _ in 1:num_spins]
+        num_Jij = size(Jij, 1)
         for i_pair = 1:num_Jij
-            i, j = Jij[i_pair][2:end]
-            push!(connection_tmp[i], Tuple(j, Jij[i_pair][3]))
-            push!(connection_tmp[j], Tuple(i, Jij[i_pair][3]))
+            i, j = Jij[i_pair][1:2]
+            push!(connection_tmp[i], (j, Jij[i_pair][3]))
+            push!(connection_tmp[j], (i, Jij[i_pair][3]))
         end
-        max_coord_num = maximum([length(connection[ispin]) for ispin in 1:num_spins])
+        max_coord_num = maximum([length(connection_tmp[ispin]) for ispin in 1:num_spins])
 
         connection = Array{Tuple{SpinIndex,Float64}}(undef, max_coord_num, num_spins)
-        coord_num = Array{UInt8}(undef, num_spins)
-        for ispin = 1:num_spins:
+        coord_num = Array{Int}(undef, num_spins)
+        for ispin = 1:num_spins
             coord_num[ispin] = length(connection_tmp[ispin])
-            connection[1:coord_num[ispin]][ispin] = connection_tmp[ispin]
+            connection[1:coord_num[ispin], ispin] = collect(connection_tmp[ispin])
         end
 
         new(num_spins, coord_num, connection)
@@ -31,7 +43,39 @@ struct SingleSpinFlipUpdater
 end
 
 
-function one_sweep(updater::SingleSpinFlipUpdater, spins::Array{Int8})
-    for ispin in 1:updater.num_spins
+
+function test(updater::SingleSpinFlipUpdater, beta::Float64, model::IsingModel, spins::Array{Int})
+    dE::Float64 = 0
+    for ispin in 1:model.num_spins
+        dE += 1.0
     end
+    return dE
+end
+
+function one_sweep(updater::SingleSpinFlipUpdater, beta::Float64, model::IsingModel, spins::Array{Int})
+    dE::Float64 = 0
+    for ispin in 1:model.num_spins
+        si_old = spins[ispin]
+
+        # Compute effective field from the rest of spins
+        eff_h::Float64 = 0.0
+        for ic in 1:updater.coord_num[ispin]
+            c = updater.connection[ic, ispin]
+            eff_h += c[2] * spins[c[1]]
+        end
+
+        # Flip spin
+        dE_ud = -2 * eff_h
+        prob_up = 1/(1 + exp(beta * dE_ud))
+        if rand() < prob_up
+            spins[ispin] = 1
+        else
+            spins[ispin] = -1
+        end
+
+        dE_tmp = - eff_h * (spins[ispin] - si_old)
+        dE += dE_tmp
+        E = compute_energy(model, spins)
+    end
+    return dE
 end
