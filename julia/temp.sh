bdcbf68b014cabf0918f15404ec90c600005d698
FumiyaKakizawa <f.kakizawa.178@ms.saitama-u.ac.jp>
2019-11-08 18:32:35 +0900

    clean up this repository.

--- julia/temp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin
-
-mkdir /home/kakizawa/performance_test
-mv  N8^3 /home/kakizawa/performance_test
-mv  N16^3 /home/kakizawa/performance_test
-mv  N32^3 /home/kakizawa/performance_test
-
-cp g.gp /home/kakizawa/performance_test
