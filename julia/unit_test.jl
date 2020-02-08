59e51d44a7e0f86f6a917cbf0886a0eadf34ecca
FumiyaKakizawa <f.kakizawa.178@ms.saitama-u.ac.jp>
2020-02-08 16:35:33 +0900

    speed up for measuring octopolar parameter

--- julia/unit_test.jl
+++ julia/unit_test.jl
@@ -13,7 +13,6 @@ a2 = 2 .* (cos(pi/3),sin(pi/3))
 
 spins = [fill((1.,0.,0.),num_spins) for i in 1:num_temps]
 rand_spins = [fill((rand(),rand(),rand()),num_spins) for i in 1:num_temps]
-println("spins: ",spins)
 
 #println("octopolar tensor: ",octopolar_orderparameter(spins,num_spins,num_temps))
 #println("unit_trinagle: ",make_kagome(num_spins))
@@ -21,5 +20,5 @@ println("spins: ",spins)
 #test for computing physical quantities.
 #@test isapprox(make_kagome(num_spins)[1][1],0.) && isapprox(make_kagome(num_spins)[1][2],0.)
 #@test isapprox(order_parameter(spins,num_spins,num_temps,(0.,0.)),[3.0])
-@test isapprox(octopolar_orderparameter(spins,num_spins,num_temps), octopolar_v2(spins,num_spins,num_temps))
+@test isapprox(octopolar_orderparameter(rand_spins,num_spins,num_temps), octopolar_v2(spins,num_spins,num_temps))
 
