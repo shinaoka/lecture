014d0cfaa89680ca6096cb90be384a0fd9c9754d
FumiyaKakizawa <f.kakizawa.178@ms.saitama-u.ac.jp>
2020-05-09 15:45:33 +0900

    temp

---
+++ julia/temp.sh
@@ -0,0 +1,5 @@
+
+cp classical_mc.jl /home/shinaoka/kagome/kakizawa
+cp mcmc.jl /home/shinaoka/kagome/kakizawa
+cp loop_update.jl /home/shinaoka/kagome/kakizawa
+cp loop_update.jl /home/shinaoka/kagome/kakizawa
