f84f753fafc7e4b183b62993bbf5fc7ecc228d53
Fumiya <kakizawafumiya@gmail.com>
2020-08-05 15:27:46 +0900

    debug non-equilibrium relaxation method.

--- julia/mk_init_non_eq_state.jl
+++ julia/mk_init_non_eq_state.jl
@@ -1,4 +1,6 @@
 
+L = 3
+
 function write_init_non_eq_state(L)
      
     num_spins = 3L^2
@@ -6,16 +8,19 @@ function write_init_non_eq_state(L)
 
         println(fp,num_spins)
         for i in 1:num_spins
-        
+       
+            site = (cos(2i*pi/3),sin(2i*pi/3),0)
             """
             theta = 10*rand()
             phi   = 10*rand()
             site  = (sin(theta)cos(phi),sin(theta)sin(phi),cos(theta))
             """
-            site = (1.,0.,0.)
-            println(fp,i," ",site[1]," ",site[2]," ",site[3])
+            #site = (1.,0.,0.)
+
+        
+            println(fp," ",site[1]," ",site[2]," ",site[3])
         end
     end
 end
 
-write_init_non_eq_state(16)
+write_init_non_eq_state(L)
